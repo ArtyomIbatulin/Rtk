720aa3d176981921438f958c14a88387a42e4b5c
ArtyomIbatulin <64503017+ArtyomIbatulin@users.noreply.github.com>
2022-05-25 18:21:16 +0300

    styled ul in users

--- react-rtk/src/features/user/UsersView.jsx
+++ react-rtk/src/features/user/UsersView.jsx
@@ -7,14 +7,18 @@ export const UsersView = () => {
   const dispatch = useDispatch()
   useEffect(() => {dispatch(fetchUsers()) } , [])
 
+const ulStyle = {
+  listStyle : 'none',
+}
+
   return (
     <div>
         <h2>List of users</h2>
         {user.loading && <div>Loading...</div>}
         {!user.loading && user.error ? <div>Error: {user.error}</div> : null}
         {!user.loading && user.users.length ? (
-          <ul>
-            {user.users.map((user) => ( <li key={user.id}>{user.name}</li>))}
+          <ul style={ulStyle}>
+            {user.users.map((user) => ( <li  key={user.id}>{user.name}</li>))}
           </ul>
         ) : null}
     </div>
